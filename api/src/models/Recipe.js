0cc2970b0545aa36b189aae7d8329da2e48f400d
Victor A. Duran B <vicduran3d@hotmail.com>
2023-01-10 17:13:58 -0500

    modelos creados

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.STRING(800),
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      defaultValue: 20
+    },
+    diets: {
+      type: DataTypes.STRING
+    },
+    steps: {
+      type: DataTypes.STRING(800),
+    },
+    image: {
+      type: DataTypes.STRING(150)
+    }
+
+  },
+    {
+      timestamps: false,
+    }
+  );
 };
